9a90ba531652f9608209c6a1219d7f6556e1cac5
Salma_Salah <salma.s.kabel@gmail.com>
2023-11-26 11:43:49 -0800

    answer

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,4 @@
-file_line {'Declare identity file':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school',
-}
-
-file_line {'Turn off passwd auth':
-  path => '/etc/ssh/ssh_config',
-  line => 'PasswordAuthentication no',
+file { '/etc/ssh/ssh_config':
+  content => 'PasswordAuthentication no
+  IdentityFile ~/.ssh/school',
 }
