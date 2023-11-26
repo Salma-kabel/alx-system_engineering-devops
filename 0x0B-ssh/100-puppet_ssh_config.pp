bd5544742513a95a704faf8accb7fca9c06dd477
Salma_Salah <salma.s.kabel@gmail.com>
2023-11-26 12:13:19 -0800

    answer

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 #using Puppet to make changes to our configuration file
 
-file_line {'Declare identity file':
+file_line {'identity file':
 path => '/etc/ssh/ssh_config',
 line => 'IdentityFile ~/.ssh/school'}
 
-file_line {'Turn off password authentication':
+file_line {'turn off password authentication':
 path => '/etc/ssh/ssh_config',
 line => 'PasswordAuthentication no'}
