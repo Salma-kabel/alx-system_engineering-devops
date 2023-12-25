619e084194ba53ee704312c7a268d47dfbf62401
Salma_Salah <salma.s.kabel@gmail.com>
2023-12-25 08:00:07 -0800

    answer

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -4,7 +4,7 @@ file { 'school':
 ensure  => 'present',
 path    => '/tmp/school',
 mode    => '0744',
-owner   => 'www-data'
-group   => 'www-data'
+owner   => 'www-data',
+group   => 'www-data',
 content => 'I love Puppet'
 }
