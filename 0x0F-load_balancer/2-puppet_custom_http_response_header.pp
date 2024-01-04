ef6d62eb9be7adf24383f4d351024f9a9c2b5c35
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-04 12:08:04 -0800

    answe

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,21 @@
+#creating a custom HTTP header response, but with Puppet.
+package {
+    'nginx':
+    ensure => installed,
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+    ensure  => file,
+    content => 'Hello World!',
+}
+
+file_line {'configure redirection':
+    path    =>  '/etc/nginx/sites-available/default',
+    after   =>  'server_name _;',
+    line    =>  "\n\tadd_header X-Served-By $HOSTNAME;\n\tlocation /redirect_me {\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n",
+}
+
+service {'nginx':
+    ensure  => running,
+    enable  => true,
+}
