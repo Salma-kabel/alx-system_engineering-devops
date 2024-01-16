f91b0376c038f17a34358a397ae38aefc0cd7a16
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-16 12:27:16 -0800

    answer

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,9 @@ def number_of_subscribers(subreddit):
     returns the number of subscribers
     """
     url = "https://www.reddit.com/r/{}/about/.json".format(subreddit)
-    res = requests.get(url, headers={'User-Agent': 'My User Agent 1.0'})
+    res = requests.get(url,
+            headers={'User-Agent': 'salma-kabel'},
+            allow_redirects=False)
     if res.status_code == 200:
         subs = res.json()
         return subs.get('data').get('subscribers')
