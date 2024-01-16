fd6ad34d1e8594fc81175658eefe27e0503b661c
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-16 14:09:20 -0800

    answer

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -16,7 +16,7 @@ def count_words(subreddit, word_list, after=None, count={}):
     else:
         url = 'https://www.reddit.com/r/{}/hot.json?after={}'.format(
             subreddit, after)
-    res = requests.get(url, headers={"user-agent": "user"},
+    res = requests.get(url, headers={"user-agent" : "user"},
                        allow_redirects=False)
     for word in word_list:
         if word.lower() not in count.keys():
