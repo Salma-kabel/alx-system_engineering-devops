4f25a8e82e3eca3abb6694cfaec934ec5e6f6c0e
Salma_Salah <salma.s.kabel@gmail.com>
2024-01-17 06:41:22 -0800

    answer

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -10,9 +10,27 @@ import requests
 
 def top_ten(subreddit):
     """gets 10 hot posts listed for a given subreddit"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    res = requests.get(url, headers={"user-agent": "SalmaSalahk"},
+    auth = requests.auth.HTTPBasicAuth('WsYGrVymVI9ASJxSjUjPow',
+                                       'CXbId8lefeCG6l9h5xbrAm4HK7HHyQ')
+    data = {
+        'grant_type': 'password',
+        'username': 'SalmaSalahk',
+        'password': '213879546',
+    }
+    headers = {
+        'User-Agent': 'python:Mandelbot v1.0.0 by /u/SalmaSalahk'
+    }
+
+    response = requests.post('https://www.reddit.com/api/v1/access_token',
+                             auth=auth, data=data, headers=headers)
+    token = response.json()['access_token']
+
+    url = "https://oauth.reddit.com/r/{}/hot".format(subreddit)
+    res = requests.get(url, headers={
+        'User-Agent': 'python:Mandelbot v1.0.0 by /u/SalmaSalahk',
+        'Authorization': f'bearer {token}'},
                        allow_redirects=False)
+
     if res.status_code == 200:
         children = res.json().get('data').get('children')
         for child in range(10):
